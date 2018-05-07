68177c38748519d6deea89aac11964b0489e00e6
Brian Tran <brian1234132@yahoo.com>
2018-05-07 04:51:12 -0700

    Add files via upload
    Could not get this done in windows. I just installed Ubuntu 10.4 via Windows Store to use on my Windows 10 desktop.
    
    You need to install Python3+ (Used Python 3.5) 
    You need to install PiP (to install python-twitter)
    Use pip to install python-twitter ("pip install python-twitter")
    
    !!!I had trouble with pip picking python2.7 by default. So I Used:
    sudo apt-get install python3-pip
    To install pip directly to my python3 install.
    Then,
    pip3 install python-twitter
    
    Compile using Ubuntu Terminal(navigate to directory, all files in same spot)
    g++ -o tweetrate -I /usr/include/python3 main.cpp -lpython3.5m
    
    Run using Ubuntu Terminal:
    ./tweetrate

---
+++ main.cpp
@@ -0,0 +1,122 @@
+//
+//  main.cpp
+//  test sentiment
+//
+//  Created by michelle natasha on 5/4/18.
+//  Copyright © 2018 michelle natasha. All rights reserved.
+//
+/*
+ The program is doing:
+ - take values of keywords from AFINN file
+ - take clean tweets file and compare to keywords value
+ - get the score for each tweet
+ - display how many positive, negative, and neutral tweets
+ */
+
+// reading a text file
+#include <python2.7/Python.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main () { // Added 5/7/18
+
+    Py_Initialize();
+    FILE* pfile = fopen("twitterpull.py", "r");
+    PyRun_SimpleFile(pfile, "twitterpull.py");
+    Py_Finalize();
+
+    cout << "\nTweetPull Successfully Completed\n" << endl;
+
+
+    //Get words and their values
+    string buffer;
+    string key[2500];
+    int val[2500];
+    ifstream myfile;
+    int count = 0;
+    double total = 0;
+    double percent = 0;
+    myfile.open("AFINN-111.txt");
+    if (myfile.is_open())
+    {
+        while (! myfile.eof() )
+        {
+            getline (myfile, buffer);
+            stringstream ss (buffer);
+            ss >> key[count] >> val[count];
+            count++;
+        }
+        myfile.close();
+    }
+    else
+        cout << "Unable to open file";
+    
+    //print words with values
+    for(int i=0;i<count;i++)
+    {
+        if(key[i] != "")
+        {
+            cout << key[i] << " " << val[i] << endl;
+        }
+    }
+    //cout << count << endl;
+    //get keywords from tweets line by line
+    int positive = 0, neutral = 0, negative = 0;
+    int score = 0;
+    int lineCount = 0;
+    string keywords[200];
+    ifstream keywordFile;
+    keywordFile.open("keyword.txt");
+    if(keywordFile.is_open())
+    {
+        while(!keywordFile.eof())
+        {
+            getline(keywordFile, buffer);
+            lineCount++;
+            for(int i=0;i<buffer.length();i++)
+                tolower(buffer[i]);
+            stringstream ss1 (buffer);
+            int wordCount=0;
+            while(ss1)
+            {
+                ss1 >> keywords[wordCount];
+                wordCount++;
+            
+                score=0;
+                for(int j=0; j<wordCount; j++)
+                {
+                    for(int k=0;k<count;k++)
+                    {
+                        if(keywords[j]==key[k])
+                        {
+                            score += val[k];
+                        }
+                    }
+                }
+            }
+                if(score >= 0)
+                    positive++;
+                else if(score==0)
+                    neutral++;
+                else
+                    negative++;
+        }
+        keywordFile.close();
+        cout << "\nThere are " << positive << " positive tweets\n";
+        cout << "There are " << neutral << " neutral tweets\n";
+        cout << "There are " << negative << " negative tweets\n";
+
+        total = positive + neutral + negative;
+        percent = positive/total;
+        percent *= 100;
+
+        cout << "\nUser has " << percent << "% " << "positive tweets" << endl;
+    }
+    else
+    cout << "Could not open keyword file" << endl;
+    return 0;
+}
---
+++ twitterpull.py
@@ -0,0 +1,16 @@
+import twitter
+
+api = twitter.Api(consumer_key = 'ilKfDdkG3AxnDk8RbMwNDQyFB',
+consumer_secret = 'sEkwwrm16xDEIELv1YIudHYVPcffOhWivaqaPdVz7srSCNebR6',
+access_token_key = '991065139582455808-gbdjeZGdxjjaqc574EcsGWPEOdNCYrT',
+access_token_secret = 'Tv2dp0eah8dy3xdW4Z3ZZwWEuX3Cz7MNFUTXxYZyLoDjb')
+
+f = open('tweets.txt', 'w', encoding='utf-8')
+
+t = api.GetUserTimeline(screen_name="realDonaldTrump", count = 50)
+tweets = [i.AsDict() for i in t]
+for tweet in tweets:
+    print('----', file = f)
+    print(tweet['text'], file = f)
+
+f.close()
